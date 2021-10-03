854902169a1db7efe030894c8e8e24df1f5d0f69
Mario <herrartemario1998@gmail.com>
2021-10-03 09:53:40 -0600

    variables

--- config/conexion.php
+++ config/conexion.php
@@ -3,16 +3,16 @@
 Class Conexion
 {
     private $db_tipo = 'mysql';
-    private $db_host = 'db-mysql-nyc3-50246-asilo-do-user-9818672-0.b.db.ondigitalocean.com';
+    private $db_host = 'localhost';
     private $db_name = 'asilosite';
-    private $db_user = 'mario';
-    private $db_pass = 'WvViqQyPvxb6uY6D';
+    private $db_user = 'root';
+    private $db_pass = '';
     private $conn;
 
     public function __construct()
     {
         try{
-            $this->conn = new PDO($this->db_tipo.':host='.$this->db_host.';dbname='.$this->db_name.';port=25060', $this->db_user, $this->db_pass);
+            $this->conn = new PDO($this->db_tipo.':host='.$this->db_host.';dbname='.$this->db_name, $this->db_user, $this->db_pass);
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         }catch(PDOException $e){
             $this->conn = "Error de conexion";
